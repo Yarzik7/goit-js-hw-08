faec89d66d249348b0992bdcaac7432b4f62054a
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-26 12:52:24 +0200

    Встановлено SimpleLightbox через npm
    + Розмітка галереї з минулого ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector(".gallery");
+const galleryNodes = []; // Масив для зберігання окремих вузлів "li"
+
+/**
+ * Функція створює розмітку елементів галереї
+ * @param {{ original, description, preview }} Деструктуризація об'єкту параметрів galleryItems
+ * @returns {void}
+ */
+const createItem = ({ original, description, preview }) => {
+  const galleryItem = document.createElement("li"); // Створення окремого елемента "li"
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__item");
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+
+  galleryLink.append(galleryImage);
+  galleryItem.append(galleryLink);
+
+  galleryNodes.push(galleryItem);
+};
+
+galleryItems.forEach((item) => createItem(item)); // Перебір даних з об'єкту galleryItems і створення елементу галереї
+galleryEl.append(...galleryNodes); // Додає готові вузли розмітки в контейнер галереї
 // Change code below this line
 
 console.log(galleryItems);
