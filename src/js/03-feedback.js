ec042ca190e4acc7ef1a72b20f4a28bd94730331
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-28 15:41:33 +0200

    Виконано завдання 8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const { email, message } = form.elements;
+const KEY = 'feedback-form-state'; // Ключ для збереження даних в локальне сховище
+
+loadingPage(); // Завантаження сторінки
+
+/**
+ * Функція при завантаженні сторінки перевіряє чи були збережені відповідні дані раніше. Якщо так то завантажує дані у відповідні поля
+ * @returns {void}
+ */
+function loadingPage() {
+  const savedData = localStorage.getItem(KEY); // Отримує збережені дані за ключем
+
+  if (!savedData) return; // Якщо збережені дані відсутні припиняє виконання функції і залишає відповідні поля пустими
+
+  try {
+    const putData = JSON.parse(savedData); // Перетворює JSON рядок в об'єкт JS
+
+    // Заповнення відповідних полей збереженими даними
+    email.value = putData.email;
+    message.value = putData.message;
+  } catch (error) {
+    console.log(`${error.name}: `, error.message); // Повідомлення про помилку в блоці try
+  }
+}
+
+/**
+ * Функція оновлює дані в локальному сховищі при введенні
+ */
+const onInputFormFields = () => {
+  const newFormData = { email: email.value, message: message.value }; // Записує введені дані в об'єкт
+
+  try {
+    localStorage.setItem(KEY, JSON.stringify(newFormData)); // Зберігає дані у вигляді JSON
+  } catch (error) {
+    console.log(`${error.name}: `, error.message); // Повідомлення про помилку в блоці try
+  }
+};
+
+
+/**
+ * 
+ * @param {event} Об'єкт події "submit"
+ */
+const onSubmitForm = event => {
+  event.preventDefault(); // Скасовує поведінку за замовчуванням
+
+  try {
+    const savedFormData = JSON.parse(localStorage.getItem(KEY)); // Отримує збережені дані за ключем
+
+    console.log(savedFormData); // Виводить збережені дані у вигляді об'єкта
+    localStorage.removeItem(KEY); // Видаляє збережені дані за ключем
+    form.reset(); // Очищення полів форми
+  } catch (error) {
+    console.log(`${error.name}: `, error.message); // Повідомлення про помилку в блоці try
+  }
+};
+
+form.addEventListener('input', throttle(onInputFormFields, 500)); // Оновлення локального сховища раз на 0.5 секунди
+form.addEventListener('submit', onSubmitForm);
