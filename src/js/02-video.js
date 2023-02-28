fed0ab013d6c4daeeb86f213837a3ee4258476f3
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-28 10:41:47 +0200

    Виконано завдання 8.2
    Додано крок збереження таймкоду в 1 секунду

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')) || 0); // Встановлює попердньо збережений таймкод відтворення відео. Значення 0 секунд за замовчуванням
+
+/**
+ * Функція зберігає в локальне сховище поточний таймкод відтворення відео 
+ * @param {event} Об'єкт події "timeupdate"
+ */
 const saveCurrentTime = event => {
   localStorage.setItem('videoplayer-current-time', event.seconds);
-  console.log(localStorage.getItem('videoplayer-current-time'));
 };
 
-player.on('timeupdate', saveCurrentTime);
-
-player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+player.on('timeupdate', throttle(saveCurrentTime, 1000)); // Обробник події оновлення часу відтворення відео. Викликає функцію для збереження таймкоду з затримкою в 1 секунду
