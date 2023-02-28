3e967abc4007ca00f4a12608d504fab659315e9a
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-28 09:55:25 +0200

    Додана можливість зберігати останній таймкод відтворення відео

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
-import Vimeo from "@vimeo/player";
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const saveCurrentTime = event => {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+  console.log(localStorage.getItem('videoplayer-current-time'));
+};
+
+player.on('timeupdate', saveCurrentTime);
+
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
